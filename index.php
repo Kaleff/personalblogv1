fbdcb6ee9efdbae114045b3f9292dea17d689320
Mark Kaliev <Marks.Kalijevs@gmail.com>
2021-04-23 05:50:21 +0300

    Played with timestamps

--- index.php
+++ index.php
@@ -13,5 +13,15 @@ if(!$connection)
     echo mysqli_connect_error();
     die();
 }
+else {
+    echo "We good <br>";
+}
+//echo "Today is ".date("d.m.Y")." and time is ".date("H:i:s")."<br>";
 
-?>
+$start_date = "2001-08-12 10:00:00";
+$start_date_timestamp = strtotime($start_date);
+
+$diff = time()-$start_date_timestamp;
+$days_passed = floor((($diff / 60) / 60) / 24);
+echo $days_passed."<br>";
+echo "Since my Birth date: " . date("d.m.Y", $start_date_timestamp) . " and current day " . date("d.m.Y") . " " .$days_passed . " days passed";
