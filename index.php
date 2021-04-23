3b70b63152e58777bd55b71d90e035698ec13526
Mark Kaliev <Marks.Kalijevs@gmail.com>
2021-04-23 06:57:13 +0300

    Added Login system

--- index.php
+++ index.php
@@ -1,27 +1,13 @@
 <?php
+define('ROOT',dirname(__FILE__));
+echo ROOT.'<br>';
+include_once(ROOT."/components/db.php");
 
-$localhost = "127.0.0.1";
-$dbUserName = "root";
-$dbPassWord = "root";
-$dbName = "test_db";
+?>
+<form method="post" action="/handler.php">
+    <input type="text" placeholder="Your login" name="user_login">
+    <input type="text" placeholder="Your password" name="user_password">
+    <hr>
+    <input type="submit" value="Sing In">
 
-$connection = mysqli_connect($localhost, $dbUserName, $dbPassWord, $dbName);
-
-if(!$connection)
-{
-    echo "Connection error <br>";
-    echo mysqli_connect_error();
-    die();
-}
-else {
-    echo "We good <br>";
-}
-//echo "Today is ".date("d.m.Y")." and time is ".date("H:i:s")."<br>";
-
-$start_date = "2001-08-12 10:00:00";
-$start_date_timestamp = strtotime($start_date);
-
-$diff = time()-$start_date_timestamp;
-$days_passed = floor((($diff / 60) / 60) / 24);
-echo $days_passed."<br>";
-echo "Since my Birth date: " . date("d.m.Y", $start_date_timestamp) . " and current day " . date("d.m.Y") . " " .$days_passed . " days passed";
+</form>
